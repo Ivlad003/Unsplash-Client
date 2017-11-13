07970fe57457ac7ee1499ba75efacef1ef76eaa8
vlad <vz@lindenvalley.de>
2017-11-13 10:26:14 +0200

    Initial Commit

---
+++ Unsplash Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unsplash Client
+//
+//  Created by Vlad Kosmach on 11/13/17.
+//  Copyright © 2017 Vlad Zatcer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
